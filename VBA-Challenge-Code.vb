3863ffd36866083b6222ce2e37a7289a80b36d75
Karina Hutula <khutula@g.clemson.edu>
2021-06-19 17:35:35 -0400

    add vba script to full stock data excel file

--- VBA-Challenge-Code.vb
+++ VBA-Challenge-Code.vb
@@ -75,6 +75,51 @@ Sub summarizeStocks()
     'call function to add headers for summarized data & max data
     Call addHeaders
     
+    'call funtion to calculate & print the max values & tickers (for bonus question)
+    Call maxValues(summarizedData())
+    
+    'call function to format all new columns/cells
+    Call formatting(rowCount + 1)
+
+End Sub
+
+Sub formatting(rowCount)
+    'format Yearly Change and Percent Change with two deicmal places & Percent Change also adds %
+    Range("J:J").NumberFormat = "0.00"
+    Range("K:K").NumberFormat = "0.00%"
+    Range("Q2:Q3").NumberFormat = "0.00%"
+    
+    'add conditional formatting for negative "Yearly Change" values (RED)
+    With Range("J2:J" & rowCount).FormatConditions.Add(xlCellValue, xlLess, "=0")
+        .Interior.ColorIndex = 3 'color cells red
+    End With
+    
+    'add conditional formatting for positive/0 "Yearly Change" values (GREEN)
+    With Range("J2:J" & rowCount).FormatConditions.Add(xlCellValue, xlGreaterEqual, "=0")
+        .Interior.ColorIndex = 4 'color cells green
+    End With
+    
+    'resize new columns
+    Columns("I:L").AutoFit
+    Columns("O:Q").AutoFit
+End Sub
+
+Sub addHeaders()
+    'add summarized data headers
+    Range("I1").Value = "Ticker Symbol"
+    Range("J1").Value = "Yearly Change ($)"
+    Range("K1").Value = "Percent Change"
+    Range("L1").Value = "Total Stock Volume"
+    
+    'adding data headers for challenge question
+    Range("O2").Value = "Greatest % Increase"
+    Range("O3").Value = "Greatest % Decrease"
+    Range("O4").Value = "Greatest Total Volume"
+    Range("P1").Value = "Ticker"
+    Range("Q1").Value = "Value"
+End Sub
+
+Sub maxValues(summarizedData())
     'create variables to track calculations
     Dim yearlyChange As Double, percentChange As Double
     
@@ -123,44 +168,4 @@ Sub summarizeStocks()
         Cells(3, 16 + i) = maxPercentDecrease(i)
         Cells(4, 16 + i) = maxTotalVolume(i)
     Next i
-    
-    'call function to format all new columns/cells
-    Call formatting(rowCount + 1)
-
-End Sub
-
-Sub formatting(rowCount)
-    'format Yearly Change and Percent Change with two deicmal places & Percent Change also adds %
-    Range("J:J").NumberFormat = "0.00"
-    Range("K:K").NumberFormat = "0.00%"
-    Range("Q2:Q3").NumberFormat = "0.00%"
-    
-    'add conditional formatting for negative "Yearly Change" values (RED)
-    With Range("J2:J" & rowCount).FormatConditions.Add(xlCellValue, xlLess, "=0")
-        .Interior.ColorIndex = 3 'color cells red
-    End With
-    
-    'add conditional formatting for positive/0 "Yearly Change" values (GREEN)
-    With Range("J2:J" & rowCount).FormatConditions.Add(xlCellValue, xlGreaterEqual, "=0")
-        .Interior.ColorIndex = 4 'color cells green
-    End With
-    
-    'resize new columns
-    Columns("I:L").AutoFit
-    Columns("O:Q").AutoFit
-End Sub
-
-Sub addHeaders()
-    'add summarized data headers
-    Range("I1").Value = "Ticker Symbol"
-    Range("J1").Value = "Yearly Change ($)"
-    Range("K1").Value = "Percent Change"
-    Range("L1").Value = "Total Stock Volume"
-    
-    'adding data headers for challenge question
-    Range("O2").Value = "Greatest % Increase"
-    Range("O3").Value = "Greatest % Decrease"
-    Range("O4").Value = "Greatest Total Volume"
-    Range("P1").Value = "Ticker"
-    Range("Q1").Value = "Value"
 End Sub
